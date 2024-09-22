ef833b5df926460e0fef4e3a39275d4d04ad1ba6
Nini <tmestrera@gmail.com>
2024-09-22 16:03:22 +0800

    worked on functionality

--- app.js
+++ app.js
@@ -2,40 +2,69 @@ const questionsBoard = ".board";
 const questionBox = ".box";
 const modalBox = ".modal";
 const modalContent = ".modal-content";
+const playerMoney = "#money";
+
+const categories = ["movies", "books", "geography", "food"];
 
 let questions = [];
+let stars = 3;
+let money = 0;
 
 function showQuestion(id) {
+  let qChosen = questions[id];
+  let choices = [];
+
+  // display
   $(modalBox).addClass("opened");
-  $(modalContent).append(`<h1>${questions[id].question}</h1>`);
+  $(modalContent).append(`<p>${qChosen.question}</p>`);
 
-  setTimeout(function () {
-    $(modalContent).html("");
+  $.each(qChosen.choices, function (index, choice) {
+    choices.push(`<div class="q-option"> ${choice} </div>`);
+  });
+
+  let chMarkup = choices.join("");
+  $(modalContent).append(`<div class="choices"> ${chMarkup} </div>`);
+
+  // check answer
+  $(".q-option").on("click", function (event) {
+    if (qChosen.correctAnswer === event.target.innerText) {
+      alert("Correct!"); // must change this
+
+      // money calculator
+      money += qChosen.amount;
+      console.log(money);
+      $(playerMoney).text(money.toString());
+    } else {
+      alert(`The correct answer is ${qChosen.correctAnswer}`);
+    }
+
+    // back to board
     $(modalBox).removeClass("opened");
-  }, 2000);
+    $(modalContent).html("");
+  });
 }
+
 function init() {
-  // fetch the json file
+  $.each(categories, function (i, category) {
+    let constMarkup = `
+        <div class="category" id="${category}"> 
+          <div class="box cat-title">${category}</div> 
+        </div>`;
+    $(questionsBoard).append(constMarkup);
+  });
+
+  // Fetch the JSON file
   $.getJSON("./questions.json", function (data) {
-    questions = data.categories;
-    $.each(questions, function (index, value) {
-      let qMarkup;
-      let qArr = [];
+    questions = data.questions;
 
-      $.each(value.questions, function (index, q) {
-        qArr.push(`<div class="box" id="q-${index}"> ${q.question} </div>`);
-      });
-
-      qMarkup = qArr.join("");
-
-      console.log(qMarkup);
-
-      const categoryTitle = `<div class="box"> ${value.category} </div>`;
-      const categoryCol = `<div class="category"> ${categoryTitle} ${qMarkup} </div>`;
-      $(questionsBoard).append(categoryCol);
+    $.each(questions, function (index, q) {
+      let questionMarkup = `<div class="q box" id="q-${index}"> ${q.amount} </div>`;
+      $(`#${q.category.toLowerCase()}`).append(questionMarkup);
     });
   }).then(function () {
-    // assign event handlers here
+    $(".q.box").on("click", function (event) {
+      showQuestion(event.target.id.slice(2));
+    });
   });
 }
 
